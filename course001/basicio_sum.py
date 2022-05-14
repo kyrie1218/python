f3dc975f3f6040c79574f4f0c1318814e3df361a
kyrie1218 <kyrie1218@163.com>
2022-05-14 21:49:51 +0800

    两整数之和

--- course001/basicio_sum.py
+++
@@ -1,6 +0,0 @@
-# 本程序用于计算两个数a, b的和
-print('This program is used for calculating the sum of two numbers, n1 and n2')
-a = int(input('Please enter the first number n1:'))
-b = int(input('Please enter the second number n2:'))
-sum2 = n1+n2
-print('The sum of n1 and n2:',sum2)
---
+++ course001/sum2.py
@@ -0,0 +1,6 @@
+# 本程序用于计算两个数a, b的和
+print('This program is used for calculating the sum of two numbers, n1 and n2')
+n1 = int(input('Please enter the first number n1:'))
+n2 = int(input('Please enter the second number n2:'))
+sum = n1+n2
+print('The sum of n1 and n2:',sum)
